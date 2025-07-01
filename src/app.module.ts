2c901186d0693b46025e5ee12221359aca2f211b
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-07-01 02:14:21 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.get('DB_HOST'),
+                port: parseInt(config.get('DB_PORT') ?? '5432', 10),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_DATABASE'),
+                autoLoadEntities: true,
+                synchronize: true,
+            })
+        }),
+    ],
 })
 export class AppModule {}
