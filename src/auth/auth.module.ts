61796501952b3571213f322f0a4a23434818adf0
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-07-25 18:21:50 -0300

    feat: protect challenge routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -14,7 +15,7 @@ import { JwtModule } from '@nestjs/jwt';
             signOptions: { expiresIn: '1d' },
         }),
     ],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy],
     controllers: [AuthController]
 })
 export class AuthModule {}
--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
 import { CreateChallengeDto } from './dto/create-challenge.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('challenge')
 export class ChallengeController {
     constructor(private readonly challengeService: ChallengeService) {}
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     async findAll() {
         return this.challengeService.findAll();
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     async create(@Body() createChallengeDto: CreateChallengeDto) {
         return this.challengeService.create(createChallengeDto);
     }
